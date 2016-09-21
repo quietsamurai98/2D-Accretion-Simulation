f4cd01f4cce0524a5ac9aaf2345a9881448065d1
quietsamurai98 <quietsamurai98@gmail.com>
2016-09-20 20:45:32 -0500

    Add files via upload

---
+++ Source/sim/Particle.java
@@ -0,0 +1,141 @@
+/**
+ * @(#)Particle.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/8/24
+ */
+import java.util.*;
+
+public class Particle {
+	private double xPosition;
+	private double yPosition;
+	private double xAcceleration;
+	private double yAcceleration;
+	private double xVelocity;
+	private double yVelocity;
+	private double xForce;
+	private double yForce;
+	private double mass;
+	private double distToCenter;
+	private double randSpin;
+	public Particle() {
+    	xPosition=0.0;
+		yPosition=0.0;
+		xAcceleration=0.0;
+		yAcceleration=0.0;
+		xVelocity=0.0;
+		yVelocity=0.0;
+		xForce=0.0;
+		yForce=0.0;
+		mass=0.0;
+		distToCenter=0.0;
+	}
+    public Particle(double diskRadius, double diskCenterX, double diskCenterY, double initialMass, double varyMass, double vel, double varyVel, double spinFactor, double varySpin, double constantGravity, double baryMass) {
+    	Random randGen = new Random();
+    	xPosition=diskCenterX;
+		yPosition=diskCenterY;
+		xAcceleration=0.0;
+		yAcceleration=0.0;
+		xForce=0.0;
+		yForce=0.0;
+		randSpin=varySpin;
+		mass=initialMass+((Math.random()*2.0-1.0)*varyMass);
+		if (!(diskRadius==0.0)){
+			double rad = Math.sqrt(randGen.nextDouble()*diskRadius)/(Math.sqrt(2)/2);
+			double theta=randGen.nextDouble()*Math.PI*2;
+			
+			double xOffset=rad*Math.cos(theta);
+			double yOffset=rad*Math.sin(theta);
+			xPosition+=xOffset;
+			yPosition+=yOffset;
+			distToCenter=Math.sqrt((xPosition-diskCenterX)*(xPosition-diskCenterX)+(yPosition-diskCenterY)*(yPosition-diskCenterY));
+			double spin=Math.sqrt((constantGravity*baryMass)/distToCenter)*spinFactor;
+			xVelocity=vel+((Math.random()*2.0-1.0)*varyVel);
+			yVelocity=vel+((Math.random()*2.0-1.0)*varyVel);
+			
+			if (Math.random()<0.5){
+				rad = randGen.nextGaussian()-randSpin;
+			}else{
+				rad = randGen.nextGaussian()+randSpin;	
+			}
+			theta=Math.atan2(yPosition,xPosition) + 0.5*Math.PI;
+			xVelocity=+rad*Math.cos(theta);
+			yVelocity=rad*Math.sin(theta);
+		}
+    }
+    public double getXPosition(){
+    	return xPosition;
+    }
+	public double getYPosition(){
+    	return yPosition;
+    }
+	public double getXAcceleration(){
+    	return xAcceleration;
+    }
+	public double getYAcceleration(){
+    	return yAcceleration;
+    }
+	public double getXVelocity(){
+    	return xVelocity;
+    }
+	public double getYVelocity(){
+    	return yVelocity;
+    }
+	public double getXForce(){
+    	return xForce;
+    }
+	public double getYForce(){
+    	return yForce;
+    }
+	public double getMass(){
+    	return mass;
+    }
+	public void setXPosition(double newXPosition){
+    	xPosition=newXPosition;
+    }
+	public void setYPosition(double newYPosition){
+    	yPosition=newYPosition;
+    }
+	public void setXAcceleration(double newXAcceleration){
+    	xAcceleration=newXAcceleration;
+    }
+	public void setYAcceleration(double newYAcceleration){
+    	yAcceleration=newYAcceleration;
+    }
+	public void setXVelocity(double newXVelocity){
+    	xVelocity=newXVelocity;
+    }
+	public void setYVelocity(double newYVelocity){
+    	yVelocity=newYVelocity;
+    }
+	public void setXForce(double newXForce){
+    	xForce=newXForce;
+    }
+	public void setYForce(double newYForce){
+    	yForce=newYForce;
+    }
+	public void setMass(double newMass){
+    	mass=newMass;
+    }
+    public void updateVel(double deltaTime){
+        xVelocity += (xForce*deltaTime)/mass;
+        yVelocity += (yForce*deltaTime)/mass;
+    }
+    public void updatePos(double deltaTime){
+	    xPosition += xVelocity*deltaTime;
+        yPosition += yVelocity*deltaTime;
+    }
+    public void updateForce(double xF, double yF){
+    	xForce += xF;
+    	yForce += yF;
+    }
+    public void zeroForce(){
+    	xForce = 0;
+    	yForce = 0;
+    }
+    public String toString(){
+    	String out = xPosition + " " + yPosition + " " + mass;
+    	return out;
+    }
+}
---
+++ Source/sim/ParticleInteraction.java
@@ -0,0 +1,253 @@
+/**
+ * @(#)ParticleInteraction.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/8/24
+ */
+import java.lang.*;
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+public class ParticleInteraction {
+        
+    /**
+     * Creates a new instance of <code>ParticleInteraction</code>.
+     */
+    
+    
+    Particle[] particleArray = new Particle[0];
+	boolean[] boolArray = new boolean[0];
+    int particleCount;
+	double initialMass;
+    double variationMass;
+   	double diskRadius;
+   	double deltaTime; //timestep in seconds
+   	double constantGravity; //gravitational constant
+   	double variationVel;
+   	double initialSpinFactor;
+   	double variationSpin;
+   	double centralParticleMass;
+   	boolean centralParticle;
+   	double randomSpin;
+   	double centerX=0;
+   	double centerY=0;
+   	int maxMassID=0;
+   	double maxMass;
+	int frameCount;
+	long startTime, elapsedTime;
+	double collisionDistanceFactor;
+	int particleNum;
+	int oldParticleNum;
+	String name;
+    public ParticleInteraction() {
+		simulate();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */    
+     private void updateVars(){
+     	Scanner kb = new Scanner(System.in);
+     	System.out.print("Simulation name                                            : ");
+		name           = kb.next(); 
+     	System.out.print("Number of initial particles (default = 1000)               = ");
+		particleCount           = kb.nextInt(); 
+		System.out.print("Initial particle mass (default = 10.0)                     = ");
+		initialMass             = kb.nextDouble(); 
+		System.out.print("Variation in initial particle mass (default = 5.0)         = ");	
+		variationMass           = kb.nextDouble(); 
+		System.out.print("Radius of the initial disk (default = 2.0)                 = ");
+		diskRadius              = kb.nextDouble(); 
+		System.out.print("Size of simulation timestep (default = 0.001)              = ");
+		deltaTime               = kb.nextDouble(); 
+		System.out.print("Gravitational Constant (default = 0.001)                   = ");
+		constantGravity         = kb.nextDouble(); 
+		System.out.print("Random variation in initial velocity (default = 0)         = ");
+		variationVel            = kb.nextDouble(); 
+		System.out.print("Random initial velocity normal to center (default = 0.5)   = ");
+		randomSpin			    = kb.nextDouble();
+		System.out.print("Is there a central particle? (default = false)             = ");
+		centralParticle         = kb.nextBoolean();
+		if (centralParticle){
+			System.out.print("Mass of the central particle (default = 100)                   = ");
+			centralParticleMass     = kb.nextDouble(); 
+		}
+		System.out.print("Collision Distance Factor (default = 1.0)                  = ");
+		collisionDistanceFactor = kb.nextDouble();
+		
+		particleNum             = particleCount;
+    } 
+    	
+    	
+    public void simulate(){
+		updateVars();
+		createDirectory();
+		spawnParticles();
+		frameCount=0;
+		startTime = System.nanoTime();
+		while(particleNum>1){
+			startTime = System.nanoTime();
+			collideParticles();
+			calculateGrav();
+	        frameCount++;
+			stepTime();
+			elapsedTime=System.nanoTime()-startTime;
+			saveText();
+	        printTime();
+		}
+    }
+
+    private void createDirectory(){
+    	File directory = new File(name+" text frames");
+		if (!directory.exists()) {
+		    boolean result = false;
+		
+		    try{
+		        directory.mkdir();
+		        result = true;
+		    } 
+		    catch(SecurityException se){
+		        //handle it
+		    }        
+		    if(result) {
+		    }
+		}else{
+			//if the directory exists, delete all existing images
+			System.out.println("Clearing directory");
+			for(File file: directory.listFiles()) 
+    			if (!file.isDirectory()) 
+        	file.delete();
+        	System.out.println("Directory cleared!");
+		}
+		String outputFileName = ".\\"+name+" text frames"+"\\"+"Settings.txt";
+        File outputFile = new File(outputFileName);
+        PrintWriter output = null;
+        try {
+        	output = new PrintWriter(outputFile);
+        }
+        catch (FileNotFoundException ex){
+        	System.out.println("File "+outputFileName+" not found.");
+        	System.exit(1);
+        }
+        
+        output.println(
+        	
+				"particleCount           = "+ particleCount + "\r\n" +
+				"initialMass             = "+ initialMass + "\r\n" +
+				"variationMass           = "+ variationMass + "\r\n" +
+				"diskRadius              = "+ diskRadius + "\r\n" +
+				"deltaTime               = "+ deltaTime + "\r\n" +
+				"constantGravity         = "+ constantGravity + "\r\n" +
+				"variationVel            = "+ variationVel + "\r\n" +
+				"initialSpinFactor       = "+ initialSpinFactor + "\r\n" +
+				"randomSpin			     = "+ randomSpin + "\r\n" +
+				"centralParticleMass     = "+ centralParticleMass + "\r\n" +
+				"centralParticle         = "+ Boolean.toString(centralParticle) + "\r\n" +
+				"collisionDistanceFactor = "+ collisionDistanceFactor
+        	
+        	);
+        output.close();
+	}
+    private void spawnParticles(){
+    	particleArray = new Particle[particleCount];
+		boolArray = new boolean[particleCount];
+		Arrays.fill(boolArray, Boolean.TRUE);    		
+        if (centralParticle){
+        	particleArray[0]=new Particle(0,0,0,centralParticleMass,0,0,0,0,0,constantGravity,centralParticleMass);
+        	for(int i=1; i<particleCount; i++){
+        		particleArray[i]=new Particle(diskRadius,0,0,initialMass,variationMass,0,variationVel,initialSpinFactor,randomSpin,constantGravity,centralParticleMass+((particleCount-1)*initialMass*0));
+        	}
+        }
+        else{
+        	for(int i=0; i<particleCount; i++){
+        		particleArray[i]=new Particle(diskRadius,0,0,initialMass,variationMass,0,variationVel,initialSpinFactor,randomSpin,constantGravity,initialMass*particleCount);
+        	}
+        }
+        maxMass=-Double.MAX_VALUE;
+    	for(int i=0; i<particleCount;i++){
+    		if (boolArray[i]&&particleArray[i].getMass()>maxMass){
+    			maxMass=particleArray[i].getMass();
+    			maxMassID=i;
+    		}
+    	}
+    }
+    private void stepTime(){
+    	oldParticleNum=particleNum;
+    	particleNum=0;
+    	for(int i=0; i<particleCount; i++){
+			if (boolArray[i]){
+		        particleArray[i].updateVel(deltaTime);
+		        particleArray[i].updatePos(deltaTime);
+		        particleArray[i].zeroForce();
+		        particleNum++;
+			}
+		}
+    }
+    private void saveText(){
+		String outputFileName = ".\\"+name+" text frames"+"\\"+String.format("%010d", frameCount)+".txt";
+        File outputFile = new File(outputFileName);
+        PrintWriter output = null;
+        try {
+        	output = new PrintWriter(outputFile);
+        }
+        catch (FileNotFoundException ex){
+        	System.out.println("File "+outputFileName+" not found.");
+        	System.exit(1);
+        }
+        
+        for(int i=0;i<particleCount;i++){
+        	output.println(particleArray[i]);
+        }
+        output.close();
+    }
+    private void printTime(){
+    	if (oldParticleNum!=particleNum)
+    		System.out.println("Frame "+String.format("%010d", frameCount)+" took " + String.format("%014d", elapsedTime) + " nanoseconds, and contained " + particleNum + " particles");
+    }
+    private void calculateGrav(){
+    	for(int i=0; i<particleCount; i++){
+			if (boolArray[i]){
+				for(int j=0; j<particleCount; j++){
+					if(i!=j&&boolArray[j]){
+						double rX = particleArray[i].getXPosition()-particleArray[j].getXPosition();
+						double rY = particleArray[i].getYPosition()-particleArray[j].getYPosition();
+						double rT = Math.sqrt(rX*rX+rY*rY);
+						double rF = rT*rT*rT;
+						double fT = -constantGravity*particleArray[i].getMass()*particleArray[j].getMass()/rF;
+						particleArray[i].updateForce(rX*fT, rY*fT);
+					}
+				}
+			}
+		}
+    }
+    private void collideParticles(){
+    	for(int i=0; i<particleCount; i++){
+			if (boolArray[i]){
+	    		for(int j=0; j<particleCount; j++){
+	    			if(boolArray[j]&&i!=j){
+	    				double massI   = particleArray[i].getMass();
+	  		  			double massJ   = particleArray[j].getMass();
+	  		  			int posXI = (int)(particleArray[i].getXPosition()*200)+400;
+	  		  			int posYI = (int)(particleArray[i].getYPosition()*200)+400;
+	  		  			int posXJ = (int)(particleArray[j].getXPosition()*200)+400;
+	  		  			int posYJ = (int)(particleArray[j].getYPosition()*200)+400;
+	    				double minDist = Math.pow(((Math.sqrt(massI)+Math.sqrt(massJ)+1)/2)*collisionDistanceFactor,2);
+	    				if (Math.pow(posXI-posXJ,2)<=minDist && Math.pow(posYI-posYJ,2)<=minDist && Math.pow(posXI-posXJ,2)+Math.pow(posYI-posYJ,2)<=minDist){
+	  		  				double massTotal = massI+massJ;
+	  		  				particleArray[i].setXVelocity(((massI*particleArray[i].getXVelocity())+(massJ*particleArray[j].getXVelocity()))/massTotal);
+	    					particleArray[i].setYVelocity(((massI*particleArray[i].getYVelocity())+(massJ*particleArray[j].getYVelocity()))/massTotal);
+	    					particleArray[i].setXPosition(((massI*particleArray[i].getXPosition())+(massJ*particleArray[j].getXPosition()))/massTotal);
+	    					particleArray[i].setYPosition(((massI*particleArray[i].getYPosition())+(massJ*particleArray[j].getYPosition()))/massTotal);
+	    					particleArray[i].setMass(massTotal);
+							particleArray[j].setXPosition(10001);
+							particleArray[j].setYPosition(10001);
+							particleArray[j].setMass(0);
+							boolArray[j]=false;
+						}
+					}
+				}
+			}
+		}
+    }
+}
